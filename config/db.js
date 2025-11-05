ac3e5c4053b907eb40495910cfa0c4a556e0ca28
naveen <97336743+Naveenak27@users.noreply.github.com>
2025-11-05 18:18:44 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 5,
     queueLimit: 0
 });
 
